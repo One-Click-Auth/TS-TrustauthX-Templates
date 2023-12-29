349595f7e489043dd0b615d60210bbde882c6145
shubham bajaj <shmbajaj32@gmail.com>
2023-12-29 12:26:04 +0530

    added react-router

--- Templates/example-reactjs-starter/src/App.tsx
+++ Templates/example-reactjs-starter/src/App.tsx
@@ -1,7 +1,32 @@
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <h1>TrusthAuthX: Example ReactJs Starter </h1>;
+  return (
+    <>
+      <aside id="sidebar">
+        <h1>Example ReactJs Starter</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/some-protected-router">A Protected Route</Link>
+            </li>
+          </ul>
+        </nav>
+      </aside>
+      <section>
+        <h1>Welcome!</h1>
+        <p>This is the home page</p>
+      </section>
+      <div id="detail"></div>
+    </>
+  );
 }
 
 export default App;
--- Templates/example-reactjs-starter/src/main.tsx
+++ Templates/example-reactjs-starter/src/main.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import RootLayout from './App.tsx';
 import './index.css';
 import { AuthContextProvider } from './context/auth-context-provider.tsx';
+import ErrorPage from './error-page.tsx';
 
 const API_KEY: string = import.meta.env.VITE_API_KEY;
 const API_SECRET: string = import.meta.env.VITE_API_SECRET;
 const ORG_ID: string = import.meta.env.VITE_ORG_ID;
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthContextProvider apiKey={API_KEY} apiSecret={API_SECRET} orgId={ORG_ID}>
-      <App />
+      <RouterProvider router={router} />
     </AuthContextProvider>
   </React.StrictMode>
 );
