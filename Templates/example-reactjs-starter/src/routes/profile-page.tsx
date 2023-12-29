5986b5afd6ce63bd4125a626e0a54b75a71549ef
shubham bajaj <shmbajaj32@gmail.com>
2023-12-29 20:10:14 +0530

    fix: missing JWT Token

--- Templates/example-reactjs-starter/src/routes/profile-page.tsx
+++ Templates/example-reactjs-starter/src/routes/profile-page.tsx
@@ -1,3 +1,20 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
+import { useUser } from '../hooks/useUser';
+
 export function ProfilePage() {
-  return <div>Profile Page</div>;
+  const location = useLocation();
+  const to = location.state?.from ?? '/';
+  const { getToken } = useAuth();
+  const { getEditProfileURL } = useUser();
+  const accessToken = getToken();
+
+  async function redirectToEditProfileULR() {
+    const editProfileURL = await getEditProfileURL(accessToken);
+    window.location.replace(editProfileURL);
+  }
+
+  redirectToEditProfileULR();
+
+  return <Navigate to={to} state={{ from: to }} replace />;
 }
