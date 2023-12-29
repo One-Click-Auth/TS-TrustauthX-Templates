1f3bae1db2befc0e0543fccd2f4074a726afe2fd
shubham bajaj <shmbajaj32@gmail.com>
2023-12-29 20:06:12 +0530

    wrapper to toggle for login component

--- Templates/example-reactjs-starter/src/components/auth-check.tsx
+++ Templates/example-reactjs-starter/src/components/auth-check.tsx
@@ -1,10 +1,12 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
 export function AuthCheck() {
   const { isSignedIn, loginURL } = useAuth();
+  const location = useLocation();
+  const to = location.state?.from ?? '/';
   if (!isSignedIn) {
     window.location.replace(loginURL);
   }
-  return <Navigate to={'/'} replace />;
+  return <Navigate to={to} state={{ from: to }} replace />;
 }
